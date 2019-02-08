26adc0666f5af57529d9602f90a429c1505fbf79
patrick.lafrance <patrick.lafrance@outlook.com>
2019-02-08 17:49:40 -0500

    enricher

--- part-1/app/amazon/src/app.jsx
+++ part-1/app/amazon/src/app.jsx
@@ -1,114 +1,11 @@
-// import React, { Component } from 'react';
-// import { connect } from "react-redux"
-// import { addItemToCart } from './actions';
-// import uuid from "uuid/v4";
-
-// // ********************************************** //
-// // ******************* CART ********************* //
-// // ********************************************** //
-
-// function Cart(props) {
-//   return (
-//     <>
-//       <p>Your Cart (<span>{props.cartAmount}$</span>)</p>
-//       <ul>
-//         {
-//           props.cartItems.map(item => {
-//             return <li key={item.id}>{item.name} - {item.price}$</li>
-//           })
-//         }
-//       </ul>
-//     </>
-//   );
-// }
-
-// export const ConnectedCart = connect(
-//   (state) => ({
-//     cartAmount: state.cartAmount,
-//     cartItems: state.cartItems
-//   }),
-//   null
-// )(Cart);
-
-
-// // ********************************************** //
-// // ******************* ITEM ********************* //
-// // ********************************************** //
-
-// function AvailableItem(props) {
-//   function handleClick(event) {
-//     event.stopPropagation();
-
-//     props.dispatch(addItemToCart(uuid(), props.name, parseInt(props.price)));
-//   }
-
-//   return (
-//     <li>{props.name} - {props.price}$ <button onClick={handleClick}>Add one item to cart</button></li>
-//   );
-// }
-
-// function AvailableItems(props) {
-//   return (
-//     <>
-//       <p>Available Items ({props.availableItems.length})</p>
-//       <ul>
-//         {
-//           props.availableItems.map(item => {
-//             return <AvailableItem key={item.id} { ...item } dispatch={props.dispatch} />
-//           })
-//         }
-//       </ul>
-//     </>
-//   );
-// }
-
-// export const ConnectedAvailableItems = connect(
-//   (state) => ({
-//     availableItems: state.availableItems,
-//   }),
-//   null
-// )(AvailableItems);
-
-// // ********************************************* //
-// // ******************* APP ********************* //
-// // ********************************************* //
-
-// export class App extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <p>Welcome to Amazon!</p>
-//         <img alt="amazon" src="https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2016/02/AMAZON-1200x537.png" />
-//         <br />
-//         <ConnectedCart />
-//         <ConnectedAvailableItems />
-//       </div>
-//     );
-//   }
-// }
-
 import React, { Component } from 'react';
 import { connect } from "react-redux"
+import { addItemToCart } from './actions';
 import uuid from "uuid/v4";
-
-import { addItemToCart, clearAllItems, removeCartItem, getItems } from './actions';
-
 
 // ********************************************** //
 // ******************* CART ********************* //
 // ********************************************** //
-
-function CartItem(props) {
-  const { item } = props;
-
-  function removeItem(event) {
-    props.dispatch(removeCartItem(item.id, item.price));
-  }
-
-  return (
-    <li key={item.id}>{item.name} - {item.price}$ <button onClick={removeItem}>Remove</button></li>
-  );
-}
 
 function Cart(props) {
   return (
@@ -117,7 +14,7 @@ function Cart(props) {
       <ul>
         {
           props.cartItems.map(item => {
-            return <CartItem key={item.id} item={item} dispatch={props.dispatch} />
+            return <li key={item.id}>{item.name} - {item.price}$</li>
           })
         }
       </ul>
@@ -139,99 +36,35 @@ export const ConnectedCart = connect(
 // ********************************************** //
 
 function AvailableItem(props) {
-  function addItem(event) {
+  function handleClick(event) {
     event.stopPropagation();
 
     props.dispatch(addItemToCart(uuid(), props.name, parseInt(props.price)));
   }
 
   return (
-    <li>{props.name} - {props.price}$ <button onClick={addItem}>Add item to cart</button></li>
+    <li>{props.name} - {props.price}$ <button onClick={handleClick}>Add one item to cart</button></li>
   );
 }
 
-class AvailableItems extends Component {
-  componentWillMount() {
-    this.props.dispatch(getItems());
-  }
-
-  clearAll(event) {
-    event.stopPropagation();
-
-    this.props.dispatch(clearAllItems());
-  }
-
-  showLoading() {
-    return (
-      <div>Loading....</div>
-    )
-  }
-
-  renderList() {
-    return (
+function AvailableItems(props) {
+  return (
+    <>
+      <p>Available Items ({props.availableItems.length})</p>
       <ul>
         {
-          this.props.availableItems.map(item => {
-            return <AvailableItem key={item.id} { ...item } dispatch={this.props.dispatch} />
+          props.availableItems.map(item => {
+            return <AvailableItem key={item.id} { ...item } dispatch={props.dispatch} />
           })
         }
       </ul>
-    );
-  }
-
-  render() {
-    return (
-      <>
-        <p>Available Items ({this.props.availableItems.length})</p>
-        <button onClick={this.clearAll}>Clear All</button>
-        {this.props.isLoading ? this.showLoading() : this.renderList()}
-      </>
-    );
-  }
+    </>
+  );
 }
-
-// function AvailableItems(props) {
-//   if (props.availableItems.length === 0) {
-//     props.dispatch(getItems());
-//   }
-
-//   function clearAll(event) {
-//     event.stopPropagation();
-
-//     props.dispatch(clearAllItems());
-//   }
-
-//   function showLoading() {
-//     return (
-//       <div>Loading....</div>
-//     )
-//   }
-
-//   function renderList() {
-//     return (
-//       <ul>
-//         {
-//           props.availableItems.map(item => {
-//             return <AvailableItem key={item.id} { ...item } dispatch={props.dispatch} />
-//           })
-//         }
-//       </ul>
-//     );
-//   }
-
-//   return (
-//     <>
-//       <p>Available Items ({props.availableItems.length})</p>
-//       <button onClick={clearAll}>Clear All</button>
-//       {props.isLoading ? showLoading() : renderList()}
-//     </>
-//   );
-// }
 
 export const ConnectedAvailableItems = connect(
   (state) => ({
     availableItems: state.availableItems,
-    isLoading: state.isLoading,
   }),
   null
 )(AvailableItems);
--- part-1/app/amazon/src/reducers.js
+++ part-1/app/amazon/src/reducers.js
@@ -1,35 +1,13 @@
-// import { ADD_ITEM } from "./actions";
-
-// const initialState = {
-//     cartAmount: 0,
-//     cartItems: [],
-//     availableItems: [
-//         { id: 1, name: "Dan Abramov - Autobiography", price: 10 },
-//         { id: 2, name: "Redux in Action", price: 5 },
-//         { id: 3, name: "Shin Megami Tensei: Strange Journey Redux", price: 1 }
-//     ]
-// }
-
-// export function rootReducers(state = initialState, { type, payload }) {
-//     switch(type) {
-//         case ADD_ITEM:
-//             return {
-//                 ...state,
-//                 cartAmount: state.cartAmount + payload.price,
-//                 cartItems: [...state.cartItems, { id: payload.id, name: payload.name, price: payload.price }]
-//             };
-//         default:
-//             return state;
-//     }
-// }
-
-import { ADD_ITEM, CLEAR_ALL, REMOVE_CART_ITEM, API_REQUEST_SUCCEEDED, API_REQUEST_SHOW_LOADING, API_REQUEST_HIDE_LOADING } from "./actions";
+import { ADD_ITEM } from "./actions";
 
 const initialState = {
     cartAmount: 0,
     cartItems: [],
-    availableItems: [],
-    isLoading: false
+    availableItems: [
+        { id: 1, name: "Dan Abramov - Autobiography", price: 10 },
+        { id: 2, name: "Redux in Action", price: 5 },
+        { id: 3, name: "Shin Megami Tensei: Strange Journey Redux", price: 1 }
+    ]
 }
 
 export function rootReducers(state = initialState, { type, payload }) {
@@ -40,37 +18,6 @@ export function rootReducers(state = initialState, { type, payload }) {
                 cartAmount: state.cartAmount + payload.price,
                 cartItems: [...state.cartItems, { id: payload.id, name: payload.name, price: payload.price }]
             };
-        case CLEAR_ALL:
-            return {
-                ...state,
-                cartAmount: 0,
-                cartItems : []
-            }
-        case REMOVE_CART_ITEM:
-            const index = state.cartItems.findIndex(x => x.id === payload.id);
-            return {
-                ...state,
-                cartAmount: state.cartAmount - payload.price,
-                cartItems: [
-                    ...state.cartItems.slice(0, index),
-                    ...state.cartItems.slice(index + 1)
-                ]
-            }
-        case API_REQUEST_SUCCEEDED:
-            return {
-                ...state,
-                availableItems: payload
-            }
-        case API_REQUEST_SHOW_LOADING:
-            return {
-                ...state,
-                isLoading: true
-            }
-        case API_REQUEST_HIDE_LOADING:
-            return {
-                ...state,
-                isLoading: false
-            }
         default:
             return state;
     }
